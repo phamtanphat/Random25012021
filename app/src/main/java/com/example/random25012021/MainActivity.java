738668354c85cf37c5f0791623ce33791f531c44
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-02-22 19:22:47 +0700

    task về nhà

--- app/src/main/java/com/example/random25012021/MainActivity.java
+++ app/src/main/java/com/example/random25012021/MainActivity.java
@@ -24,15 +24,22 @@ public class MainActivity extends AppCompatActivity {
 //        int a = 10;
 
         // Kiểu dữ liệu đối tượng (Khởi tạo vùng nhớ và có địa chỉ)
-        Random random = new Random();
+//        Random random = new Random();
+//
+//        // Xử lý random trong 5 -> 10
+//        for (int i = 0; i < 50; i++) {
+//            int value = random.nextInt(10 - 5 + 1) + 5;
+//            Log.d("BBB","Giá trị = " + value);
+//        }
 
-        // Xử lý random trong 5 -> 10
-        for (int i = 0; i < 50; i++) {
-            int value = random.nextInt(10 - 5 + 1) + 5;
-            Log.d("BBB","Giá trị = " + value);
-        }
 
 
+        // Yêu cầu về nhà
+        // Task 1 : Kiểm tra người dùng phải nhập đủ số min và số max
+        // Task 2 : Kiểm tra giá trị max không được bé hơn hoặc bằng số min
+        //                    Xử lý : smax = smin + 1
+        // Task 3 : Xử lý random trong khoảng số max và số min
+        // Task 4 : Hiển thị giá trị random lên textview
 
 
 
