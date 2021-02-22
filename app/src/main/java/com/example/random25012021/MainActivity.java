f8092dde56c45d1a2ee5dcd03e0890d6e18deaa5
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-02-22 18:03:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/random25012021/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.random25012021;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.Person;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Biến toàn cục
+    String mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Biến cục bộ
+        String textView;
+
+        // Đối tượng
+        Person person1
+    }
+    // Hàm (Method)
+    private void setLayout(){
+
+    }
+
+    private void getData(){
+
+    }
+}
